ee9762f7988e3db90729b11d721dec11eaa70c22
xSKYx123 <57019594+xSKYx123@users.noreply.github.com>
2019-11-01 19:49:55 -0300

    html/js
    kk

---
+++ index.js
@@ -0,0 +1,28 @@
+var x = parseInt(prompt("Primer numero de tu suma"));
+var y = parseInt(prompt("segundo numero de tu suma"));
+var resultado = suma (x,y);
+alert (resultado);
+function suma (x,y) {
+    var i = 2;
+    return i + x + y;
+}
+
+var a = parseInt(prompt("primer numero de tu multiplicacion"));
+var s = parseInt(prompt("segundo numero de tu multiplicacion"));
+var resultado = multiplicar (a,s)
+alert (resultado); 
+function multiplicar (a,s) {
+    var i = 1
+    return i * a * s;
+}
+
+var a = parseInt(prompt("primer numero de tu resta"));
+var f = parseInt(prompt("segundo numero de tu resta"));
+var resultado = resta (a,f)
+alert (resultado);
+function resta (a,f) {
+return a - f 
+}
+
+ 
+ 
